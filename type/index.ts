7c9b77ba30f4a8520836354ec34189b175ee4079
notcis <notcis07@gmail.com>
2025-05-28 21:26:53 +0700

    secert

--- type/index.ts
+++ type/index.ts
@@ -3,3 +3,5 @@ import { z } from "zod";
 
 export type LoginType = z.infer<typeof loginFormSchema>;
 export type AddFriendType = z.infer<typeof addFriendFormSchema>;
+
+//AUTH_SECRET="J4KGPcOUAeWzS6P8fPYMTmM7ams9E6H9mLDqOZ4L9c8=" # Added by `npx auth`. Read more: https://cli.authjs.dev
