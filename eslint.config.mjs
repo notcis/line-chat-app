9e99376facc2dc77548a8a90ca8eb373de3ecdca
notcis <notcis07@gmail.com>
2025-05-29 09:09:03 +0700

    fix  server

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  // ✅ Ignore Prisma auto-generated files
+  {
+    ignores: ["lib/generated/prisma/**"],
+  },
 ];
-
 export default eslintConfig;
