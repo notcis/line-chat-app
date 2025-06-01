aacddc4d0cfc95ab558dd732a3fa50afccb72c8c
notcis <notcis07@gmail.com>
2025-06-01 12:06:41 +0700

    change admin

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -2,16 +2,22 @@ import { hashSync } from "bcrypt-ts-edge";
 export const sampleData = {
   users: [
     {
-      email: "notcis07@gmail.com",
-      username: "Notcis",
-      password: hashSync("49310407", 10),
-      lineId: "123456",
+      email: "admin@coopmsds.com",
+      username: "Admin",
+      password: hashSync("coop2012", 10),
+      lineId: "123456789",
     },
     {
       email: "ploy@gmail.com",
       username: "Ploy Kitta",
       password: hashSync("49310407", 10),
-      lineId: "654321",
+      lineId: "987654321",
+    },
+    {
+      email: "notcis07@gmail.com",
+      username: "Notcis",
+      password: hashSync("49310407", 10),
+      lineId: "456987123",
     },
   ],
 };
--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -8,5 +8,5 @@ export const LIST_CONVERSATIONS = "list_conversations";
 export const MESSAGES = "messages";
 export const CONVERSATION = "conversation";
 
-export const ADMIN_ID = "cde734ba-1139-421a-b606-7c2c115d011b";
+export const ADMIN_ID = "8d6d3792-2176-49a0-a834-412513c2307e";
 export const LINE_MESSAGE_TYPE = ["text", "image"];
